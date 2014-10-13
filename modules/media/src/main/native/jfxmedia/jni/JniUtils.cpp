3e273de181a822134cf2f71c1091fc1981e7149f
jgiles <jgiles@localhost>
2014-10-13 14:15:46 -0700

    RT-38516: [DialogPane] ContentText sometimes not displayed

---
+++ modules/media/src/main/native/jfxmedia/jni/JniUtils.cpp
@@ -0,0 +1,134 @@
+/*
+ * Copyright (c) 2010, 2014, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+#include "JniUtils.h"
+#include "Logger.h"
+#include <Common/ProductFlags.h>
+
+#if ENABLE_PLATFORM_GSTREAMER
+#include <platform/gstreamer/GstJniUtils.h>
+#endif // ENABLE_PLATFORM_GSTREAMER
+
+void ThrowJavaException(JNIEnv *env, const char* type, const char* message)
+{
+    // First check if there's a pending exception, if there is then do nothing
+    if (env->ExceptionCheck()) {
+        return;
+    }
+
+    jclass klass = NULL;
+    if (type) {
+        klass = env->FindClass(type);
+        if (!klass) {
+            // might have caused an exception
+            if (env->ExceptionOccurred()) {
+                env->ExceptionClear();
+            }
+        }
+    }
+    if (!klass) {
+        klass = env->FindClass("java/lang/Exception");
+        if (!klass) {
+            if (env->ExceptionOccurred()) {
+                env->ExceptionClear();
+            }
+            // This shouldn't happen...
+            return;
+        }
+    }
+    env->ThrowNew(klass, message);
+}
+
+JNIEnv *GetJavaEnvironment(JavaVM *jvm, jboolean &didAttach)
+{
+    JNIEnv *env = NULL;
+
+    didAttach = false;
+    if (jvm->GetEnv((void**)&env, JNI_VERSION_1_4) != JNI_OK) {
+        didAttach = true;
+        jvm->AttachCurrentThreadAsDaemon((void**)&env, NULL);
+    }
+    return env;
+}
+
+bool CJavaEnvironment::hasException()
+{
+    return (bool)environment->ExceptionCheck();
+}
+
+bool CJavaEnvironment::clearException()
+{
+    if (environment->ExceptionCheck()) {
+        environment->ExceptionClear();
+        return true;
+    }
+    return false;
+}
+
+/**
+ * Check whether there is a pending exception and if so, log its string version
+ * and return true, otherwise, i.e., if there is no exception, return false.
+ */
+bool CJavaEnvironment::reportException()
+{
+    jthrowable exc = environment->ExceptionOccurred();
+    if (NULL != exc) {
+        jclass cid = environment->FindClass("java/lang/Throwable");
+        jmethodID mid = environment->GetMethodID(cid, "toString", "()Ljava/lang/String;");
+        jstring jmsg = (jstring)environment->CallObjectMethod(exc, mid);
+        char* pmsg = (char*)environment->GetStringUTFChars(jmsg, NULL);
+        LOGGER_ERRORMSG(pmsg);
+        environment->ReleaseStringUTFChars(jmsg, pmsg);
+        environment->ExceptionClear();
+        environment->DeleteLocalRef(exc);
+        environment->DeleteLocalRef(cid);
+        return true;
+    }
+    return false;
+}
+
+CJavaEnvironment::CJavaEnvironment(JavaVM *jvm) :
+attached(false),
+environment(NULL)
+{
+    if (NULL != jvm) {
+        environment = GetJavaEnvironment(jvm, attached);
+    }
+}
+
+CJavaEnvironment::~CJavaEnvironment()
+{
+    if (attached && environment) {
+        JavaVM *jvm;
+        if (environment->GetJavaVM(&jvm) == JNI_OK) {
+            jvm->DetachCurrentThread();
+        }
+    }
+}
+
+JNIEnv *CJavaEnvironment::getEnvironment()
+{
+    return environment;
+}
---
+++ modules/web/src/main/java/com/sun/javafx/webkit/prism/WCLinearGradient.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2011, 2014, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package com.sun.javafx.webkit.prism;
+
+import com.sun.prism.paint.LinearGradient;
+import com.sun.prism.paint.Stop;
+import com.sun.webkit.graphics.WCGradient;
+import com.sun.webkit.graphics.WCPoint;
+import com.sun.javafx.geom.transform.BaseTransform;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+final class WCLinearGradient extends WCGradient<LinearGradient> {
+
+    private final WCPoint p1;
+    private final WCPoint p2;
+    private final List<Stop> stops = new ArrayList<Stop>();
+
+    WCLinearGradient(WCPoint p1, WCPoint p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    @Override
+    protected void addStop(int argb, float offset) {
+        this.stops.add(new Stop(WCGraphicsPrismContext.createColor(argb), offset));
+    }
+
+    public LinearGradient getPlatformGradient() {
+        Collections.sort(this.stops, WCRadialGradient.COMPARATOR);
+        return new LinearGradient(
+                this.p1.getX(),
+                this.p1.getY(),
+                this.p2.getX(),
+                this.p2.getY(),
+                BaseTransform.IDENTITY_TRANSFORM,
+                isProportional(),
+                getSpreadMethod() - 1, // convert webkit to prism
+                this.stops);
+    }
+
+    @Override
+    public String toString() {
+        return WCRadialGradient.toString(this, this.p1, this.p2, null, this.stops);
+    }
+}
